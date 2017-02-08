6195849dcd78acbe8de9b878bfcc20eabf7c634b
Sodoku Solver <sodokusolver5@gmail.com>
2017-02-08 13:20:49 +1300

    five-by-five

--- js/common-ui.js
+++ js/common-ui.js
@@ -2,6 +2,8 @@ var ZERO = "0".charCodeAt(0)
 var NINE = "9".charCodeAt(0)
 var LOWER_A = "a".charCodeAt(0)
 var UPPER_A = "A".charCodeAt(0)
+var LOWER_Y = "y".charCodeAt(0)
+var UPPER_Y = "Y".charCodeAt(0)
 var LOWER_Z = "z".charCodeAt(0)
 var UPPER_Z = "Z".charCodeAt(0)
 var BACKSPACE = 8
@@ -29,6 +31,12 @@ function getGrid(dimx,dimy){
         }
     }
     return grid
+}
+
+function coords(cell){
+    var coords = cell.id.split("-")
+    var x = parseInt(coords[0]),  y = parseInt(coords[1])
+    return [x,y]
 }
 
 /*
@@ -60,7 +68,7 @@ function focusPrev(that){
 /*
   Handle an arrow key, focussing up or down, left or right.
 */
-function focusDirection(event,key){
+function focusDirection(dimx, dimy, event,key){
     var id = $( event.currentTarget ).attr("id")
     var coords = id.split("-" )
     var x = parseInt(coords[0]), y = parseInt(coords[1])
@@ -69,60 +77,109 @@ function focusDirection(event,key){
         y = (y - 1 >= 0) ? y - 1 : y
         break;
     case DOWN:
-        y = (y + 1 < 9) ? y + 1 : y
+        y = (y + 1 < dimy) ? y + 1 : y
         break;
     case LEFT:
         x = (x - 1 >= 0) ? x - 1 : x
         break;
     case RIGHT:
-        x = (x + 1 < 9) ? x + 1 : x
+        x = (x + 1 < dimx) ? x + 1 : x
         break;
     }
     $("#" + x + "-" + y).focus()
 }
 
 /*
-  Generate a key handler function that uses the given acceptfn to decide 
-  whether to allow the keypress to register in content. 
-  acceptfn should return null if keypress is not accepted as content otherwise 
-  the content it wants in the cell.
+  Generate a key handler function that uses the given acceptfn to decide
+  whether to allow the keypress to register in content.
+
+  dimx - width of grid in cells
+
+  dimy - height of grid in cells
+
+  acceptfn (optional) - should return null if keypress is not accepted as content otherwise
+  it should return the content it wants in the cell.
+
+  queuefn (optional) should return a reference to a list to push the key on to
+
+  contentfn (optional) can be used to provide a custom html setter, it is passed
+    the target element and the accepted contents
+
+  undofn - function to call when
 */
-function gridKeyHandlerFactory(acceptfn, queueslot){
+function gridKeyHandlerFactory(dimx, dimy, options){
 
     return function(event) {
         var k = event.keyCode || event.charCode
-        if(k == TAB) return
 
-        event.preventDefault()
+        var acceptfn = null, queuefn = null, contentfn = null
+        if(options){
+            acceptfn = options.accept
+            queuefn = options.queue
+            contentfn = options.contenthandler
+        }
+
+        if(k != TAB){
+            event.preventDefault()
+        }
 
         var accept = acceptfn(event)
-        if(accept){
-            $( event.currentTarget ).html( accept )
-
+        if(accept != null){            
+            if(contentfn){
+                contentfn(event.currentTarget, accept)
+            } else {
+                $( event.currentTarget ).html( accept )
+            }
+            $( event.currentTarget ).focus()
+            
         } else if(k == SPACE ) {
             $( event.currentTarget ).html("")
             focusNext($(this))
-
+            
         } else if(k == RETURN ) {
             focusNext($(this))
-
+            
         } else if(k == BACKSPACE ) {
             // Backspace will clear and focus previous square
             $( event.currentTarget ).html("")
             focusPrev($(this))
-
+            
         } else if(k == DEL) {
             // DEL clears current square
             $( event.currentTarget ).html("")
-
+            
         } else if(k == UP || k == DOWN || k == LEFT || k == RIGHT) {
-            focusDirection(event, k)
-        }
-
-        if(queueslot) {
-            queueslot().push(k)
+            focusDirection(dimx, dimy, event, k)
+            
         }
     }
+    
+    if(queuefn) {
+        queuefn().push(k)
+    }
+}
+
+function renderResults(results){
+    if(results && results.length > 0){
+        console.log( results )
+        var html = "<ol>"
+        for(var i = 0; i < results.length; ++i){
+            var word = results[i]
+            html += '<li><a target="_blank" href="http://www.thefreedictionary.com/'+ word +'">' + word + '</a></li>'
+        }
+        html += "</ol>"
+
+        $("#results").html(html)
+    }
+}
+
+function acceptUpperLetters(event){
+    var k = event.keyCode || event.charCode
+    var result = null
+    if(k >= LOWER_A && k <= LOWER_Z || k >= UPPER_A && k <= UPPER_Z){
+        result = event.key.toUpperCase()
+    }
+    return result
 }
 
 function flash(clazz){
@@ -141,4 +198,3 @@ function flashGrid(){
     flash("alert")
     setTimeout(function(){ puzzle.toggleClass("alert"); }, delay * 4)
 }
-
---
+++ js/five-by-five-ui.js
@@ -0,0 +1,103 @@
+function getGridQueries(){
+    var across = [], down = [], seen = {}
+
+    function addCell(x, y, query){
+        var val = '?'
+        var cell = $("#" + x + "-" + y)
+        var text = cell.text()
+        var index = cell.attr("index")
+        if(text){
+            val = text.charAt(0).toLowerCase()
+            if(seen[val]) index = seen[val]
+            else seen[val] = index
+            cell.attr("index", index)
+        } 
+        query.push([ index, val])
+    }
+    
+    // across
+    for(var y = 0; y < 5; ++y){
+        var query = []
+        across.push(query)
+        for(var x = 0; x < 5; ++x) addCell(x, y, query)
+    }
+
+    // down
+    for(var x = 0; x < 5; ++x){
+        var query = []
+        down.push(query)
+        for(var y = 0; y < 5; ++y) addCell(x, y, query)
+    }
+    
+    return across.concat(down)
+}
+
+function applySolution(solution){
+    for(var y = 0; y < 10; ++y){
+        for(var x = 0; x < 5; ++x){
+            var part = solution[y][x]
+            $("#" + x + "-" + y).html(part[1].toUpperCase())
+        }
+    }
+}
+
+function showWords(solution){
+    var words = []
+    for(var y = 0; y < 10; ++y){
+        var word = ""
+        for(var x = 0; x < 5; ++x){
+            var part = solution[y][x]
+            word += part[1]
+        }
+        words.push(word)
+    }
+    renderResults(words)
+}
+
+function solveHandler(event){
+
+    disable()
+    var queries = getGridQueries()
+    console.log("queries:", prettyQueries( queries ) )
+    
+    var solved = solve( queries )
+    console.log("solution:", prettyQueries( solved ) )
+    if(solved){
+        applySolution( solved ) 
+        showWords( solved ) 
+    } else {
+        flashGrid()
+    }
+    
+    enable()
+}
+
+
+function enable(){
+    $("#solve").prop("disabled", false)
+}
+
+function disable(){
+    $("#solve").prop("disabled", true)
+}
+
+function initGrid(){
+
+    $("#puzzle-grid tr td").attr("contenteditable","true")
+    $("#puzzle-grid tr td").addClass("cell")
+    $(".cell").keydown(gridKeyHandlerFactory(5, 5, {
+        "accept" : acceptUpperLetters
+    }))
+    $("#solve").click(solveHandler)
+    
+    var index = 0
+    for(var y = 0; y < 5; ++y){
+        for(var x = 0; x < 5; ++x){
+            var id = x + "-" + y
+            var cell = document.getElementById( id )
+            $(cell).attr("index", ++index)
+        }
+    }
+}
+
+$(function(){ initGrid(); })
